fec39105396f4d9a1ddc9af60bd9fcaa9e0a6dc6
Anton Lenev <m0rg0t.Anton@gmail.com>
2018-07-20 17:31:38 +0300

    updated probtn

--- showinparent_concat.js
+++ showinparent_concat.js
@@ -27,19 +27,34 @@ function getParameterByName(name) {
 }
 
 var params = {};
+
+var current_params = document.getElementById("probtn_additional_params");
+if ((current_params!==null) && (current_params!==undefined)) {
+	var json = JSON.parse(current_params.innerText);
+	params = json;
+}
+
 params.dfp = {};
 params.dfp.isDFP = true;
 params.dfp.clickUrlEsc = getParameterByName("click_url_esc");
 params.dfp.cacheBuster = getParameterByName("cacheBuster");
-var domain = getParameterByName("domain");
-if ((domain!==null) && (domain!==undefined) && (domain!=="")) {
-	params.domain = domain;
+
+var getParamFromUrl = function(name) {
+	var getParam = getParameterByName(name);
+	if ((getParam!==null) && (getParam!==undefined) && (getParam!=="")) {
+		params[name] = getParam;
+		return getParam;
+	}
 }
 
-//selectAdSet param
-var SelectAdSet = getParameterByName("SelectAdSet");
-if ((SelectAdSet!==null) && (SelectAdSet!==undefined) && (SelectAdSet!=="")) {
-	params.SelectAdSet = SelectAdSet;
+var domain = getParamFromUrl('domain');
+var SelectAdSet = getParamFromUrl('SelectAdSet');
+getParamFromUrl('CreativeId');
+getParamFromUrl('UseGeoLocation');
+getParamFromUrl('WaitForGeoLocation');
+
+if ((domain===null) || (domain===undefined) || (domain==="")) {
+	domain = document.domain.replace("www.", "");
 }
 
 var paramsDiv = window.top.document.createElement('div');
@@ -55,8 +70,14 @@ try {
 } catch (ex) {
 }
 
-
-loadJS('//cdn.probtn.com/probtn_concat.js', function () {
-});
+if ((domain === "babyblog.ru") || (domain === "m.babyblog.ru")) { //eception for babyblog
+	if ((SelectAdSet!==null) && (SelectAdSet!==undefined) && (SelectAdSet!=="")) {
+		loadJS('//cdn.probtn.com/probtn_concat.js', function () {});
+	} else {
+		console.log("empty SelectAdSet at babyblog");
+	}
+} else {
+	loadJS('//cdn.probtn.com/probtn_concat.js', function () {});
+}
 
 })();
